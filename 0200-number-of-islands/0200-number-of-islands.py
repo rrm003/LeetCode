0b0d30996d03e33bdf0c2e907ad4dd376e9c648f
rrm003 <31697080+rrm003@users.noreply.github.com>
2024-11-01 17:46:29 -0500

    Time: 251 ms (46.23%), Space: 20.2 MB (43.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,40 @@
 class Solution:
-    def bfs(self, grid, i, j, visited):
-        l = len(grid)
-        w = len(grid[0])
-
+    def traverse(self, grid, x, y, lookup):
+        row = len(grid)
+        col = len(grid[0])
+        neighbors = [(1, 0), (-1, 0), (0, -1), (0, 1)]
         queue = []
-        queue.append((i, j))
-        neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        queue.append((x, y))
 
         while len(queue) > 0:
-            x, y = queue[0]
+            curr = queue[0]
             queue = queue[1:]
-            visited.add((x, y))
-            
-            for n in neighbours:
-                nx, ny = x+n[0], y+n[1]
-                if (nx >=0 and nx < l and ny>=0 and ny<w):
-                    if  grid[nx][ny] == "1" and  (nx, ny) not in visited:
-                        queue.append((nx, ny))
-                        visited.add((nx, ny))
+            lookup[curr[0]][curr[1]] = True
 
+            for n in neighbors:
+                i, j = curr[0] + n[0], curr[1] + n[1]
+                if i < 0 or i >= row or j < 0 or j >= col:
+                    continue
+
+                if lookup[i][j] or grid[i][j] == "0":
+                    continue
+
+                queue.append((i, j))
+                lookup[i][j] = True
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        islands = 0
+        row = len(grid)
+        col = len(grid[0])
 
-        for i, row in enumerate(grid):
-            for j, col in enumerate(row):
-                if grid[i][j] == "1" and (i, j) not in visited:
-                    self.bfs(grid, i, j, visited)
-                    # print(visited)
-                    islands+=1
-        
-        return islands
-        
+        lookup = [ [False for x in range(col)] for y in range(row)]
+        print(lookup)
+
+        island = 0
+        for i in range(row):
+            for j in range(col):
+                if lookup[i][j] or grid[i][j] == "0":
+                    continue
+                else:
+                    self.traverse(grid, i, j, lookup)
+                    island += 1
+        return island
