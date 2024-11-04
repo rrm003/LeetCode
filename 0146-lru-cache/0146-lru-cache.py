0a1bbf3e307185d31671820cca7620bb19e80ce1
rrm003 <31697080+rrm003@users.noreply.github.com>
2024-11-04 04:52:11 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,116 +1,76 @@
 class Node:
-    def __init__(self, key: int, value: int, left = None, right = None):
-        self.key = key 
-        self.value = value 
-        self.next = left
-        self.prev = right
+    def __init__(self, key, value, prev = None, next = None):
+        self.key = key
+        self.val = value
+        self.prev = prev
+        self.next = next
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.lookup = {}
         self.head = None
         self.tail = None
-        self.lookup = {}
-    
-    def log(self):
-        pass
-        # print("------------")
-        # print("lookup")
-        # for x, v in self.lookup.items():
-        #     print(f"\t{x}:({v.key}:{v.value})")
-        # print("------------")
-        # print("list")
-        # s = ""
-        # tt = self.head
-        # while tt!=None:
-        #     s =  s + "->" + str(tt.key)
-        #     tt = tt.next
-        # print(s)
+        self.cap = capacity
 
     def get(self, key: int) -> int:
-        # print("------------")
-        # print("get", key)
         if key not in self.lookup:
-            # self.log()
             return -1
         
-        curr = self.lookup[key] 
-        if curr == self.head:
-            # self.log()
-            return curr.value
-        
+        curr = self.lookup[key]
+        prev = curr.prev
+        next = curr.next
+
         if curr == self.tail:
-            self.tail = curr.prev
+            self.tail = prev
 
-        p = curr.prev
-        curr.prev = None
-
-        n = curr.next
+        if prev != None:
+            prev.next = next
+        
+        if next != None:
+            next.prev = prev
+        
+        if self.head != None:
+            self.head.prev = curr
+        
         curr.next = self.head
-        
-        self.head.prev = curr
         self.head = curr
-        
-        p.next = n
-        if n:
-            n.prev = p
-        
-        
-        # self.log()
-        return curr.value
+
+        return curr.val
 
     def put(self, key: int, value: int) -> None:
-        # print("------------")
-        # print("put", key, value)
-        if self.head == None:
-            # print("\t head is none")
-            self.head = Node(key, value)
-            self.tail = self.head
-            self.lookup[key] = self.head
-            # self.log()
-            return
-        
         if key in self.lookup:
-            # print("\t key exists")
-            curr = self.lookup[key]
-            curr.value = value
-            if curr == self.head:
-                # self.log()
-                return
-            if curr == self.tail:
-                self.tail= curr.prev
-
-            p = curr.prev
-            n = curr.next
-        
-            p.next = n
-            if n:
-                n.prev = p
-
-            curr.prev = None
-            curr.next = self.head
-            self.head.prev = curr
-            self.head = curr
-            # self.log()
+            node = self.lookup[key]
+            node.val = value
+            self.get(key)
             return
+        
+        if len(self.lookup) < self.cap:
+            node = Node(key, value)
+            self.lookup[key] = node
+            node.next = self.head
+            if self.head == None:
+                self.head = node
+                self.tail = node
+                return 
 
-        # print("\t key not found")
-        curr = Node(key, value)
-        curr.next = self.head
-        self.head.prev = curr
-        self.head = curr
+            self.head.prev = node
+            self.head = node
 
-        if len(self.lookup) == self.capacity:
-            del self.lookup[self.tail.key]
+            return
+        
+        del self.lookup[self.tail.key]
+        self.tail = self.tail.prev
+        self.tail.next = None
 
-            pt = self.tail.prev
-            if pt != None:
-                pt.next = None
-            self.tail = pt
+        node = Node(key, value)
+        self.lookup[key] = node
+        node.next = self.head
+        self.head.prev = node
+        self.head = node
+        
 
-        self.lookup[key] = curr
-        # self.log()
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
