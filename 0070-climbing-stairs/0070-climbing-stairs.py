ab16536375ddd8f349380c3959ff25d0713df474
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-02-24 00:16:43 -0600

    Time: 0 ms (100%), Space: 17.5 MB (95.68%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climb(self, n: int, dp: map) -> int:
+        if n < 0: return 0
+        if n == 0: return 1
+        if n in dp : return dp[n]
+
+        rslt = 0
+        rslt += self.climb(n-1, dp)
+        rslt += self.climb(n-2, dp)  
+
+        dp[n] = rslt
+
+        return rslt
+
+    def climbStairs(self, n: int) -> int:
+        dp = {}
+        rslt = self.climb(n, dp)
+        return rslt
