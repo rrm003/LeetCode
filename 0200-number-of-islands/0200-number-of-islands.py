756def4c0487209d722be2e2924355df9420ba80
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-03-11 03:22:17 -0500

    Time: 258 ms (45.77%), Space: 25.4 MB (28.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,40 +1,31 @@
+from collections import deque
+
 class Solution:
-    def traverse(self, grid, x, y, lookup):
-        row = len(grid)
-        col = len(grid[0])
-        neighbors = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-        queue = []
-        queue.append((x, y))
-
-        while len(queue) > 0:
-            curr = queue[0]
-            queue = queue[1:]
-            lookup[curr[0]][curr[1]] = True
-
-            for n in neighbors:
-                i, j = curr[0] + n[0], curr[1] + n[1]
-                if i < 0 or i >= row or j < 0 or j >= col:
-                    continue
-
-                if lookup[i][j] or grid[i][j] == "0":
-                    continue
-
-                queue.append((i, j))
-                lookup[i][j] = True
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
+        neighbours = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-        lookup = [ [False for x in range(col)] for y in range(row)]
-        print(lookup)
-
-        island = 0
-        for i in range(row):
-            for j in range(col):
-                if lookup[i][j] or grid[i][j] == "0":
-                    continue
-                else:
-                    self.traverse(grid, i, j, lookup)
-                    island += 1
-        return island
+        i = 0 
+        visited = set()
+        island_count = 0
+        while i < m :
+            j = 0
+            while j < n:
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    queue = deque([])
+                    queue.append((i, j))
+                    visited.add((i, j))
+                    island_count += 1
+                    while queue:
+                        x, y = queue.popleft()
+                        for (nx, ny) in neighbours:
+                            x1, y1 = x+nx, y+ny
+                            if x1 < 0 or x1>=m or y1<0 or y1>=n:
+                                continue
+                            if (x1, y1) not in visited and grid[x1][y1] == "1":
+                                queue.append((x1, y1))
+                                visited.add((x1, y1))
+                j += 1
+            i += 1
+        return island_count
