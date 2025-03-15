709c3c772d2c0e59b75fc5e04a8b986482146601
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-03-14 23:35:47 -0500

    Time: 3 ms (39.16%), Space: 17.7 MB (94.73%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,28 +1,19 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        rslt = []
-
-        i = 0
-        p = 0
+        i = 0 
         l = len(chars)
-
+        curr = 0
         while i < l:
-            curr = chars[i]
-            count = 1
-
+            chars[curr] = chars[i]
+            curr += 1
             j = i + 1
-            while j < l and curr == chars[j]:
-                count+=1
-                j += 1
-
-            chars[p] = curr
-            p += 1
-            if count > 1: 
-                freq = str(count)
-                for f in freq:
-                    chars[p] = f
-                    p += 1
-
+            while j < l and chars[j] == chars[i]:
+                j+=1
+            
+            if j - i > 1: 
+                for x in str(j-i):
+                    chars[curr] = x
+                    curr += 1
             i = j
-        
-        return p
+
+        return curr
