e1be60db534917ec1593afb9b175d83907137e8d
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-03-22 16:37:11 -0500

    Time: 5 ms (60.15%), Space: 18.9 MB (83.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,18 +1,15 @@
 class Solution:
-    def dfs(self, graph, curr, completed, visits) -> bool:
+    def dfs(self, graph, curr, completed) -> bool:
         if len(graph[curr]) == 0: return True
-        if completed[curr]: return True
-        if curr in visits: return False
+        if curr in completed: return completed[curr]
 
         rslt = True
-        visits.append(curr)
+        completed[curr] = False
         for deps in graph[curr]:
-            rslt = rslt and self.dfs(graph, deps, completed, visits)
-        visits = visits[:-1]
+            rslt = rslt and self.dfs(graph, deps, completed)
 
         completed[curr] = rslt
         return rslt
-
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {}
@@ -20,13 +17,12 @@ class Solution:
 
         for i in range(numCourses):
             graph[i] = [] 
-            completed[i] = False
         
         for preq in prerequisites:
             graph[preq[0]].append(preq[1])
 
         rslt = True
         for i in range(numCourses):
-            rslt = rslt and self.dfs(graph, i, completed, [])
+            rslt = rslt and self.dfs(graph, i, completed)
         
         return rslt
