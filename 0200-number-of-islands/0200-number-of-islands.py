29775d928fac395c96ecc52c8e4a594918403443
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-03-22 15:48:55 -0500

    Time: 257 ms (46.85%), Space: 20.3 MB (58.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,37 @@
-from collections import deque
-
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def bfs(self, grid, i, j, visited):
         m = len(grid)
         n = len(grid[0])
-        neighbours = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-        i = 0 
-        visited = set()
-        island_count = 0
-        while i < m :
-            j = 0
-            while j < n:
-                if grid[i][j] == "1" and (i, j) not in visited:
-                    queue = deque([])
-                    queue.append((i, j))
-                    visited.add((i, j))
-                    island_count += 1
-                    while queue:
-                        x, y = queue.popleft()
-                        for (nx, ny) in neighbours:
-                            x1, y1 = x+nx, y+ny
-                            if x1 < 0 or x1>=m or y1<0 or y1>=n:
-                                continue
-                            if (x1, y1) not in visited and grid[x1][y1] == "1":
-                                queue.append((x1, y1))
-                                visited.add((x1, y1))
-                j += 1
-            i += 1
-        return island_count
+        neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        queue = []
+        queue.append((i, j))
+
+        while queue:
+            x, y = queue[0]
+            queue = queue[1:]
+
+            visited[x][y] = True
+
+            for neighbour in neighbours:
+                nx, ny = x + neighbour[0], y + neighbour[1]
+                if nx < 0 or nx >= m or ny < 0 or ny >= n: continue
+
+                if grid[nx][ny] == "1" and visited[nx][ny] == False:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+
+        island = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and visited[i][j] == False:
+                    self.bfs(grid, i, j, visited)
+                    island += 1
+
+        return island
