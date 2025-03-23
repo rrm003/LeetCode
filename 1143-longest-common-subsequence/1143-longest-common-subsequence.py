6d45b632a7a6148896f7b37fc0b6c59467ee21c9
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-03-22 22:57:03 -0500

    Time: 471 ms (55.47%), Space: 43.6 MB (27.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,20 +1,20 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-
-    def lcs(self, text1, text2, i, j):
-        key = f"{i},{j}"
-        if key in self.memo:
-            return self.memo[key]
-
-        if i == len(text1) or j == len(text2):
-            return 0
-        elif text1[i] == text2[j]:
-            self.memo[key] = 1+self.lcs(text1, text2, i+1, j+1)
-            return self.memo[key]
-        else:
-            self.memo[key] = max(self.lcs(text1, text2, i+1, j), self.lcs(text1, text2, i, j+1))
-            return self.memo[key]
-
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        return self.lcs(text1, text2, 0, 0)
+        m = len(text1) + 1
+        n = len(text2) + 1
+
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        # print(dp)
+
+        for i, ci in enumerate(text1):
+            i += 1
+            for j, cj in enumerate(text2):
+                j+=1
+                if ci == cj:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                
+
+        return dp[-1][-1]
+
