cc8812c29f51293b2f455c7f7195019bc5bdcbbf
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-04-05 16:05:51 -0500

    Time: 23 ms (7.28%), Space: 19.1 MB (72.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,30 @@
 class Solution:
-    def dfs(self, graph, curr, completed) -> bool:
-        if len(graph[curr]) == 0: return True
-        if curr in completed: return completed[curr]
-
+    def dfs(self, course, graph, satisfied, stack)-> bool:
+        if satisfied[course]: return True
+        if course in stack: return False
+        
+        stack.append(course)
         rslt = True
-        completed[curr] = False
-        for deps in graph[curr]:
-            rslt = rslt and self.dfs(graph, deps, completed)
+        for neighbour in graph[course]:
+            rslt = rslt and self.dfs(neighbour, graph, satisfied, stack)
 
-        completed[curr] = rslt
+        satisfied[course] = rslt
         return rslt
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {}
-        completed = {}
-
-        for i in range(numCourses):
-            graph[i] = [] 
+        satisfied = {}
+        for i in range(0, numCourses):
+            graph[i] = []
+            satisfied[i] = False
         
-        for preq in prerequisites:
-            graph[preq[0]].append(preq[1])
+        for c1, c2 in prerequisites:
+            graph[c1].append(c2)
 
-        rslt = True
-        for i in range(numCourses):
-            rslt = rslt and self.dfs(graph, i, completed)
+        for i in range(0, numCourses):
+            if not satisfied[i]:
+                rslt = self.dfs(i, graph, satisfied, [])
+                if not rslt : return False
+                satisfied[i] = rslt
         
-        return rslt
+        return True
