d90cceae8cbc5f70a80db14cdcb147dfb7a2b224
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-04-10 10:13:32 -0500

    Time: 330 ms (36.55%), Space: 18.8 MB (71.96%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,35 +1,54 @@
-from collections import deque as dq
+# queue = [lot, dog, log] 
+
+# dog
+# 1 : [h, d, l, c]
+# cog
+
+# 2 : [i, o]
+# dig
+
+# 3 : [t, g]
+
+# visited = [hit, hot, dot, dog]
+from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        lookup = {}
-        l = len(beginWord)
+        word_lookup = {}
+        level_lookup = {}
 
-        word_set = set(wordList)
+        n = len(beginWord) # len(beginWord) == len(endWord)
 
-        for i in range(l):
-            lookup[i] = set()
+        # mark all the word not visited
+        for word in wordList:
+            word_lookup[word] = False
         
-        for i in range(l):
-            for word in word_set:
-                lookup[i].add(word[i])
 
-        queue = dq([])
-        queue.append((beginWord, 1))
-        visited = {beginWord: 1}
+        # get characters available at each level
+        for i in range(n):
+            level_lookup[i] = set()
+            for word in wordList:
+                level_lookup[i].add(word[i]) 
+
+        for i in range(n):
+            level_lookup[i].add(beginWord[i])
+
+
+        # print(word_lookup)
+        # print(level_lookup)
+
+        queue = deque()
+        queue.append((beginWord, 0))
+        word_lookup[beginWord] = True
 
         while queue:
-            curr, lvl = queue.popleft()
-            
-            if curr == endWord: return lvl
-            
-            visited[curr] = lvl
-            
-            i, l = 0, len(curr)
-            for i in range(l):
-                for c in lookup[i]:
-                    new_word = curr[:i] + c + curr[i+1:]
-                    if new_word != curr and new_word not in visited and new_word in word_set:
-                        queue.append((new_word, lvl + 1))
-                        visited[new_word] = lvl + 1
+            # print(queue)
+            curr, level = queue.popleft()
+            if curr == endWord: return level + 1
+            for i in range(n):
+                for x in level_lookup[i]:
+                    new_word = curr[:i] + x + curr[i+1:]
+                    if new_word in word_lookup and not word_lookup[new_word]:
+                        word_lookup[new_word] = True
+                        queue.append((new_word, level + 1))
         return 0
