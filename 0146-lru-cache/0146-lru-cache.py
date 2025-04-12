efd5191e4ea9893446a77fb6aa973bb9e3248708
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-04-12 12:35:15 -0500

    Time: 131 ms (68.03%), Space: 78.6 MB (27.42%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,76 +1,62 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__ (self, key: int, val: int):
         self.key = key
-        self.value = val
+        self.val = val
         self.next = None
-        self.prev = None
+        self.prev = None 
+
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.lookup = {}
-        self.head = Node(-1, 0)
-        self.tail = Node(-1, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head 
-        self.size = capacity
+        self.Size = capacity
+        self.Head = Node(-1, -1)
+        self.Tail = Node(-1, -1)
+        self.Head.next = self.Tail
+        self.Tail.prev = self.Head
+        self.Lookup = {}
 
     def get(self, key: int) -> int:
-        if key not in self.lookup: return -1
-        node = self.lookup[key]
+        if key not in self.Lookup:
+            return -1
+        
+        node = self.Lookup[key]
         prev, next = node.prev, node.next
         prev.next = next
         next.prev = prev
 
-        f = self.head.next
-        f.prev = node
-        node.next = f
+        first = self.Head.next 
+        self.Head.next = node 
+        node.prev = self.Head
+        node.next = first
+        first.prev = node
 
-        self.head.next = node
-        node.prev = self.head
-        
-        return node.value
-
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.lookup:
-            node = self.lookup[key]
-            node.value = value
-
-            prev, next = node.prev, node.next
-            prev.next = next
-            next.prev = prev
-
-            f = self.head.next
-            f.prev = node
-            node.next = f
-
-            self.head.next = node
-            node.prev = self.head
-
-            return node.value
+        if key in self.Lookup:
+            self.Lookup[key].val = value
+            self.get(key)
+            return
         
-        if len(self.lookup) == self.size:
-            p = self.tail.prev
-            del self.lookup[p.key]
-            p.prev.next = p.next
-            p.next.prev = p.prev
+        if len(self.Lookup) == self.Size:
+            evict = self.Tail.prev
+            prev = evict.prev
+            prev.next = self.Tail
+            self.Tail.prev = prev
+            del self.Lookup[evict.key]
         
-        node = Node(key, value)
-        self.lookup[key] = node
+        new_node = Node(key, value)
+        self.Lookup[key] = new_node
+        next = self.Head.next
+        self.Head.next = new_node
+        new_node.prev = self.Head 
+        new_node.next = next
+        next.prev = new_node
 
-        f = self.head.next
-        f.prev = node
-        node.next = f
-
-        self.head.next = node
-        node.prev = self.head
-
-        return
 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-# (4, 1) (2, 3) 
