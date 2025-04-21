3eecec2d7a48e3e3f053319ea5ceb660b91c7a1a
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-04-21 18:41:46 -0500

    Time: 4 ms (64.58%), Space: 17.9 MB (21.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,56 +1,43 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        num = 0
         l = len(s)
-        number = 0
 
         i = 0
-        while i < l :
-            c = s[i]
-            if c == "I":
-                if i < len(s) - 1 and s[i+1] == "V":
-                    number += 4
-                    i+=1
-                elif i < len(s) - 1 and s[i+1] == "X":
-                    number += 9
-                    i+=1
-                else:
-                    number += 1
-
-            elif c == "V":
-                # if i < len(s) - 1 and s[i+1] == "I":
-                #     number += 6
-                # else: 
-                number += 5
-                
-            elif c == "X":
-                if i<len(s) - 1 and s[i+1] == "L":
-                    number += 40
-                    i+=1
-                elif  i<len(s) - 1 and s[i+1] == "C":
-                    number += 90
-                    i+=1
-                else:
-                    number += 10
-
-            elif c == "L":
-                number += 50
-
-            elif c == "C":
-                if i<len(s) - 1 and s[i+1] == "D":
-                    number += 400
+        while i < len(s):
+            if s[i] == "I":
+                if i + 1 < l and s[i+1] == "V":
+                    num += 4
                     i += 1
-                elif i<len(s) - 1 and s[i+1] == "M":
-                    number += 900
+                elif i + 1 < l and s[i+1] == "X":
+                    num += 9
                     i += 1
-                else:
-                    number += 100
-
-            elif c == "D":
-                number += 500
-
-            elif c == "M":
-                number += 1000
-
-            i += 1
+                else: num += 1
+            elif s[i] == "V":
+                num += 5
+            elif s[i] == "X":
+                if i + 1 < l and s[i+1] == "L":
+                    num += 40
+                    i += 1
+                elif i + 1 < l and s[i+1] == "C":
+                    num += 90
+                    i += 1
+                else: num += 10
+            elif s[i] == "L":
+                num += 50
+            elif s[i] == "C":
+                if i + 1 < l and s[i+1] == "D":
+                    num += 400
+                    i += 1
+                elif i + 1 < l and s[i+1] == "M":
+                    num += 900
+                    i += 1
+                else: num += 100
+            elif s[i] == "D":
+                num += 500
+            elif s[i] == "M":
+                num += 1000
+            
+            i+=1
         
-        return number
+        return num
