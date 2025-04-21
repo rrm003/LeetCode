f12049db7d95835921e81c98a0fa4277dfe1a841
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-04-21 18:10:15 -0500

    Time: 22 ms (6.04%), Space: 18.2 MB (40.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,36 +1,43 @@
+import heapq as hq
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        l1 = len(nums1)
-        l2 = len(nums2)
-
-        m = (l1 + l2) // 2 
-        # 8 8 -> 8 & 7
-        # 8 7 -> 7
-
-        pt_list1 = 0
-        pt_list2 = 0
-        pt = 0
-        f, s = 0, 0
-        while pt <= m:
-            if pt_list1 >= l1 or pt_list2 >= l2:
-                if pt_list1 < l1:
-                    f = s 
-                    s = nums1[pt_list1] 
-                    pt_list1 += 1
-                elif pt_list2 < l2: 
-                    f = s
-                    s = nums2[pt_list2]
-                    pt_list2 += 1
-            elif nums1[pt_list1] <= nums2[pt_list2]:
-                f = s
-                s = nums1[pt_list1]
-                pt_list1 += 1
-            elif nums1[pt_list1] > nums2[pt_list2]:
-                f = s 
-                s = nums2[pt_list2]
-                pt_list2 += 1
-            
-            pt+=1
+        left  = []
+        right = []
         
-        return s if (l1 + l2) % 2 else (f + s) / 2
-# 1 1 3 3 4 4 5
+        m = len(nums1)
+        n = len(nums2)
+
+        i = 0 
+        j = 0 
+
+        while i < m or j < n:
+            curr = 0
+            if i < m and j < n:
+                if nums1[i] < nums2[j]:
+                    curr = nums1[i] 
+                    i+=1
+                else:
+                    curr = nums2[j]
+                    j+=1
+            elif i < m:
+                curr = nums1[i]
+                i+=1
+            elif j < n:
+                curr = nums2[j]
+                j+=1
+
+            hq.heappush(left, -curr)
+            max_left = -hq.heappop(left)
+            hq.heappush(right, max_left)
+            if len(right) >= len(left) + 1:
+                min_right = hq.heappop(right)
+                hq.heappush(left, -min_right)
+        
+        # print(left, right)
+        if (m+n) % 2 == 0:
+            m1 = -hq.heappop(left)
+            m2 = hq.heappop(right)
+            return (m1 + m2) / 2
+
+        return -hq.heappop(left)
