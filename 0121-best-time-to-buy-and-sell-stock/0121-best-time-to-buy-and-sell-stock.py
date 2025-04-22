a00f810a1f5fcba016a30036cbc7218bcd6e4bf5
rrm003 <31697080+rrm003@users.noreply.github.com>
2025-04-22 02:30:54 -0500

    Time: 247 ms (5.15%), Space: 27.3 MB (9.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-
-        stock = []
-
-        for price in prices:
-            if len(stock) == 0:
-                stock.append(price)
-                continue
-            
-            while stock and stock[-1] > price:
-                stock = stock[:-1]
-            
-            if stock:
-                max_profit = max(max_profit, price - stock[0])
-            
-            stock.append(price)
+        stack = []
+        stack.append(prices[0])
         
-        return max_profit
+        profit = 0
+        for price in prices[1:]:
+            while stack and stack[-1] > price:
+                stack = stack[:-1]
+            
+            if stack:
+                profit = max(profit, price - stack[0])
+            
+            stack.append(price)
+        
+        # print(stack)
+
+        return profit
